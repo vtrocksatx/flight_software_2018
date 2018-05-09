bcbe14ffb7e5e0f2f6e3e155a2c1f9b8dc99dd9c
Anthony DeFilippis <tonydefilippis@users.noreply.github.com>
2018-05-09 19:15:06 +0000

    Update serialEcho.py

--- Testing/Python/serialEcho.py
+++ Testing/Python/serialEcho.py
@@ -1,22 +1,38 @@
+import os
 import serial
 
-serialPort = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=3.0)
+#Set serial
+serialPort = serial.Serial("/dev/ttyS0", baudrate=19200, timeout=3.0)
 
+
+#Open and increment boot counter
+bootCountFile = open('bootCountFile', 'r')
+bootCount = int(bootCountFile.read())
+bootCountFile.close()
+bootCountFile = open('bootCountFile', 'w')
+bootCountFile.write(str(bootCount+1))
+bootCountFile.close()
+
+#Create directory for log files
+path = './Boot_Number_' + str(bootCount) + '/'
+os.system('mkdir Boot_Number_' + str(bootCount) )
+fileName = path + 'file_0'
+logFile = open(fileName, 'wb')
+
+#Variables
 current = 0; #keeps track of loop
 fileNum = 0; #track file number
-threshhold = 256
-filename = 'file' + str(fileNum)
-logFile = open(filename, 'wb')
+threshhold = 271
 
+
+#Loop to read in data
 while True:
-    #rcv = serialPort.readline()
-    #print(rcv)
     readCharacters = serialPort.read()
     current += len(readCharacters)
     if current > threshhold:
         logFile.close() #close previous file before starting up a new file
         fileNum += 1
-        fileName = 'file' + str(fileNum)
+        fileName = path + 'file_' + str(fileNum)
         logFile = open(fileName, 'wb')
         logFile.write(readCharacters) 
         current = len(readCharacters)
